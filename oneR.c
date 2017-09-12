f2f175221e2bd691fb115c73cbb41809bd6c7dd8
Robert Francalangia <rcfran17@g.holycross.edu>
2017-09-12 18:34:34 -0400

    check email.1.patch

--- oneR.c
+++ oneR.c
@@ -4,12 +4,12 @@
  *  Created By : rfrancalangia                                     
  *  
  *  Creation Date : 09-12-2017
- *  Last Modified : Tue Sep 12 18:31:16 2017
+ *  Last Modified : Tue Sep 12 18:34:15 2017
  *  Description : This will be the main file for the oneR algo       
  *                               
  * *************************************************************** */
 
 int main(int argc, char **argv)
 {
-	
+	return (0);
 }
